02245893c5977bc21d37cffbdd91617e91feb0b8
aun raza <aun313raza@gmail.com>
2024-07-26 01:22:09 +0500

    list assigment

---
+++ main.dart
@@ -0,0 +1,7 @@
+void main() {
+  //Q.1) Create a list of names and print all names using list.
+  print("List Of Names.");
+  List<String> names = ["ahmed", "fayaz", "akbar"];
+  
+  print(names);
+}
