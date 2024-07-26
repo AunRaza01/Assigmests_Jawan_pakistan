73a4999a31b798986416d60a23ce6f6969822750
aun raza <aun313raza@gmail.com>
2024-07-26 16:19:03 +0500

    List Assgiments

--- main.dart
+++ main.dart
@@ -1,7 +1,20 @@
 void main() {
   //Q.1) Create a list of names and print all names using list.
-  print("List Of Names.");
+  //Q. 2) Create a list of Days and print only  Sunday
+  //Q.3)  create a list  of name, class, roll no, grade, percentage. And print.
+  print("Assigment of Lists");
   List<String> names = ["ahmed", "fayaz", "akbar"];
-  
+  List<String> days = [
+    "saturday",
+    "sunday",
+    "monday",
+    "tuesday",
+    "wednesday",
+    "thursday",
+    "friday"
+  ];
+  List<dynamic> information = ["ahmed", "12th", "4578", "A+", "87%"];
   print(names);
+  print(days[1]);
+  print(information);
 }
